b70554e56a801cc887f300adb95c722ff8e6ace3
Omkar <omkarbaacha@foreignadmits.com>
2022-07-30 14:27:11 +0530

    sidebar project added.

---
+++ 05-sidebar/app.js
@@ -0,0 +1,21 @@
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', function () {
+  console.log(sidebar.classList);
+
+  //method1
+  // if (sidebar.classList.contains('show-sidebar')) {
+  //   sidebar.classList.remove('show-sidebar');
+  // } else {
+  //   sidebar.classList.add('show-sidebar');
+  // }
+
+  //method2
+  sidebar.classList.toggle('show-sidebar')
+});
+
+closeBtn.addEventListener('click',function(){
+  sidebar.classList.remove('show-sidebar')
+})
