d409d4a659f811225ec53d1739b80b1eeae6aeda
Omkar <omkarbaacha@foreignadmits.com>
2022-07-30 14:28:10 +0530

    modal project added

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -2,3 +2,16 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+
+const modalBtn = document.querySelector('.model-btn')
+const modalOverlay = document.querySelector('.modal-overlay')
+const modalClose = document.querySelector('.close-btn')
+
+modalBtn.addEventListener('click',()=>{
+  modalOverlay.classList.add('open-modal')
+})
+
+modalClose.addEventListener('click',()=>{
+  modalOverlay.classList.remove('open-modal')
+})
